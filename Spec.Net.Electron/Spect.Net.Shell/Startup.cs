248a5c63be9d5c8903837724730afe7d723e659b
Istvan Novak <dotneteer@hotmail.com>
2019-11-23 15:54:12 +0100

    Website reduced to minimum.

--- Spec.Net.Electron/Spect.Net.Shell/Startup.cs
+++ Spec.Net.Electron/Spect.Net.Shell/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Spect.Net.Shell.Data;
 
 namespace Spect.Net.Shell
 {
@@ -24,7 +23,6 @@ namespace Spect.Net.Shell
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
