6a61099777d9f7f6fe8405afaf6557c41231c011
Istvan Novak <dotneteer@hotmail.com>
2019-12-30 13:58:04 +0100

    Unused files removed.

--- Spec.Net.Electron/Spect.Net.Shell.Shared/content/Messaging.ts
+++
@@ -1,8 +0,0 @@
-﻿/**
- * This interface represents an abstract message type that can be
- * either a request or a response.
- */
-export interface AbstractMessage {
-    command: string;
-    correlationId?: number;
-}
