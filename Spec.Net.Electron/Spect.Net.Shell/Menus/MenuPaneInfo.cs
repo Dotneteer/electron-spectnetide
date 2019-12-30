4dd7545ecfd0a3d334fd76edec1a30e3ff7f8d16
Istvan Novak <dotneteer@hotmail.com>
2019-12-30 17:52:50 +0100

    Tooling stabilized.

---
+++ Spec.Net.Electron/Spect.Net.Shell/Menus/MenuPaneInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Spect.Net.Shell.Menus
+{
+    /// <summary>
+    /// This class describes information about menu panes opened in the main menu
+    /// </summary>
+    public class MenuPaneInfo
+    {
+        /// <summary>
+        /// The index of the item in the parent, if this is a submenu pane
+        /// </summary>
+        public int ParentIndex { get; set; }
+
+        /// <summary>
+        /// Menu items in the pane
+        /// </summary>
+        public List<MenuItem> Items { get; set; }
+
+        /// <summary>
+        /// Left position in pixels
+        /// </summary>
+        public int LeftPos { get; set; }
+
+        /// <summary>
+        /// Top position in pixels
+        /// </summary>
+        public int TopPos { get; set; }
+
+        /// <summary>
+        /// The index of the selected item
+        /// </summary>
+        public int SelectedIndex { get; set; }
+    }
+}
--- Spec.Net.Electron/Spect.Net.Shell/State/AppState.cs
+++ Spec.Net.Electron/Spect.Net.Shell/State/AppState.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using Spect.Net.Shell.Menus;
+﻿using Spect.Net.Shell.Menus;
+using System.Collections.Generic;
 
 namespace Spect.Net.Shell.State
 {
@@ -14,14 +14,19 @@ namespace Spect.Net.Shell.State
         public WindowState WindowState { get; set; }
 
         /// <summary>
+        /// Indicates if a modal dialog is displayed
+        /// </summary>
+        public bool IsModelDisplayed { get; set; }
+
+        /// <summary>
         /// Indicates if the main window has the focus.
         /// </summary>
         public bool HasFocus { get; set; }
 
         /// <summary>
-        /// The current menu to display
+        /// The state of the menu
         /// </summary>
-        public Menu Menu { get; set; }
+        public MenuState MenuState { get; set; }
 
         /// <summary>
         /// The intial app state to use when intializing the stores
@@ -30,24 +35,62 @@ namespace Spect.Net.Shell.State
         {
             WindowState = WindowState.Normal,
             HasFocus = true,
-            Menu = new Menu()
+            MenuState = new MenuState
             {
-                Items = new List<IMenuItem>
+                Menu = new Menu()
                 {
-                    new MenuItem("file")
+                    Items = new List<IMenuItem>
                     {
-                        Label = "&File"
-                    },
-                    new MenuItem("edit")
-                    {
-                        Label = "&Edit"
-                    },
-                    new MenuItem("view")
-                    {
-                        Label = "&View"
+                        new MenuItem("file")
+                        {
+                            Label = "&File"
+                        },
+                        new MenuItem("edit")
+                        {
+                            Label = "&Edit"
+                        },
+                        new MenuItem("view")
+                        {
+                            Label = "&View"
+                        }
                     }
-                }
+                },
+                SelectedIndex = -1,
+                HighlightAccessKeys = false,
+                OpenPanes = new List<MenuPaneInfo>(),
+                KeyboardAction = false
             }
         };
+    }
+
+    /// <summary>
+    /// This class represents the state of the menu
+    /// </summary>
+    public sealed class MenuState
+    {
+        /// <summary>
+        /// The collection of menu items
+        /// </summary>
+        public IMenu Menu { get; set; }
+
+        /// <summary>
+        /// The index of the pointed menu button in the menu bar
+        /// </summary>
+        public int SelectedIndex { get; set; }
+
+        /// <summary>
+        /// Indicates if access keys should be highlighted
+        /// </summary>
+        public bool HighlightAccessKeys { get; set; }
+
+        /// <summary>
+        /// Open menu panes
+        /// </summary>
+        public List<MenuPaneInfo> OpenPanes { get; set; }
+
+        /// <summary>
+        /// Indicates if the last action was a keyboard action
+        /// </summary>
+        public bool KeyboardAction { get; set; }
     }
 }
